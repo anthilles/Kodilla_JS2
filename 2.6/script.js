f9b928bd183342a07ff1af63539b396b03f0f0d8
anthilles <anthilles@outlook.com>
2017-09-19 14:11:39 +0200

    2.6 Zadanie: Niezależne liczniki

---
+++ 2.6/script.js
@@ -0,0 +1,39 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter -1
+        });
+    },
+
+    render: function() {
+        return React.createElement(
+            'h2',{},this.state.counter,
+            React.createElement(
+                'button',
+                {onClick: this.increment},
+                React.createElement('p', {}, 'Dodawanie ')
+            ),
+                React.createElement(
+                'button',
+                {onClick: this.decrement},
+                React.createElement('p', {}, 'Odejmowanie ')
+            )
+        );
+    }
+});
+
+var element = React.createElement(Counter);
+
+ReactDOM.render(element, document.getElementById('app'));
